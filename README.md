237a3654c8256835014aeb4654a3120adf955306
Brooks Patton <brooks@brookzerker.com>
2019-09-01 18:48:44 -0600

    basic game screen created

---
+++ README.md
@@ -0,0 +1,43 @@
+# Pong
+
+## Technologies
+
+- Rust
+- GGEZ
+
+## Todo
+
+* [ ] Create basic game screen
+
+## Skills needed
+
+* _ as a catch all
+* _ to mark variables not used
+* :: on Structs
+* . on functions
+* Adding a dependency
+* Adding multiple dependencies with ::{...}
+* Creating new project
+* enums
+* expect()
+* Functions
+* Implemented functions for structures
+* Implicit return
+* Installing Rust
+* Main function in main file
+* Match
+* Mutable vs immutable variables
+* printing to the console
+* Public vs private methods
+* references
+* Result type
+* Running a project
+* self
+* Semi-colons are required
+* Setting up VS Code
+* String literals
+* Structs
+* Traits
+* Tuples
+* turbofish syntax
+* Variables
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use ggez::{ContextBuilder, event, Context, GameResult, graphics};
+use ggez::event::{EventHandler};
+
+struct Pong {
+
+}
+
+impl Pong {
+    pub fn new() -> Pong {
+        Pong {}
+    }
+}
+
+impl EventHandler for Pong {
+    fn update(&mut self, _context: &mut Context) -> GameResult<()> {
+        Ok(())
+    }
+
+    fn draw(&mut self, context: &mut Context) -> GameResult<()> {
+        graphics::clear(context, graphics::BLACK);
+
+        graphics::present(context)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let (mut context, mut event_loop) = ContextBuilder::new("rusty_pong", "Brooks Patton")
+        .build()
+        .expect("game context not able to be created :(");
+
+    let mut pong = Pong::new();
+
+    match event::run(&mut context, &mut event_loop, &mut pong) {
+        Ok(_) => println!("Game finished, hope you had fun!"),
+        Err(error) => println!("Error playing the game: {}", error)
+    };
 }
